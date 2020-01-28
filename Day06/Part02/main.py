668faa5b5676505a33f0ee91ad110ddc8dfa1e57
John Kemp <jkemp@matthewsbooks.com>
2020-01-28 16:19:13 -0600

    * Finished Day 06 Part 02 * Applied some changes from part 2 to part 1

--- Day06/Part02/main.py
+++ Day06/Part02/main.py
@@ -4,15 +4,36 @@ from anytree import Node, RenderTree, AsciiStyle, PostOrderIter
 
 def main():
     runFile("Day06/map.txt")
-    # runFile("Day06/Part01/sample_42.txt")
+    # runFile("Day06/Part02/sample_4.txt")
 
 
 def runFile(filename):
     program = load_file(filename)
-    tree = create_tree(program)
+    list = create_tree_list(program)
+
+    you = list["YOU"]
+    san = list["SAN"]
+
+    # alternate traversal from each end until depth is same?
+    yd = you.parent  # start with item you is orbiting
+    sd = san.parent  # start with item san is orbiting
+    count = 0
+    while yd.name != sd.name:
+        if yd.depth > sd.depth:
+            print(f"YOU at ({yd.name},{yd.depth}). Going down")
+            yd = yd.parent
+            count += 1
+        else:
+            print(f"SAN at ({sd.name},{sd.depth}). Going down")
+            sd = sd.parent
+            count += 1
+
+    print(f"SAN at ({sd.name},{sd.depth}). SAN at ({sd.name},{sd.depth})")
+    print(f"Transfers: {count}")
+
     #renderTreeToFile(filename, tree)
-    nodes = countNodes(tree)
-    print(f"Nodes: {nodes}")
+    #nodes = countNodes(tree)
+    #print(f"Nodes: {nodes}")
 
 
 def renderTreeToFile(filename, tree):
@@ -21,28 +42,24 @@ def renderTreeToFile(filename, tree):
         f.write("%s%s\r" % (pre, node.name))
 
 
-def create_tree(program):
+def create_tree_list(program):
     list = {}
     for n in program:
         centerName = n[0]
         center = list.get(centerName)
         if(center == None):
-            #print(f"C Miss: {centerName}")
             center = Node(centerName)
             list[centerName] = center
 
         orbiterName = n[1]
         orbiter = list.get(orbiterName)
         if(orbiter == None):
-            #print(f"O Miss: {orbiterName}")
             orbiter = Node(orbiterName, parent=center)
             list[orbiterName] = orbiter
         elif orbiter.parent == None:
             orbiter.parent = center
 
-    tree = list["COM"]
-
-    return tree
+    return list
 
 
 def countNodes(tree):
